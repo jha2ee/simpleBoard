d7c81da92243234b8c30b3be6147ee6020086df5
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-07-13 13:01:28 +0900

    json 라이브러리 추가
    라이브러리 및 api 추가

--- board-web/src/main/java/com/study/boardweb/Controller/HelloWorldController.java
+++ board-web/src/main/java/com/study/boardweb/Controller/HelloWorldController.java
@@ -1,8 +1,12 @@
 package com.study.boardweb.Controller;
 
+import org.json.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -12,5 +16,16 @@ public class HelloWorldController {
     public String test() {
         // 여기에다가 샘플 데이터 넣기
         return "Hello, world!";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/getBoard")
+    public String getBoard() throws JSONException {
+        JSONObject post = new JSONObject();
+        post.put("id", "i1");
+        post.put("title", "t1");
+
+        // 여기에다가 샘플 데이터 넣기
+        return post.toString();
     }
 }
