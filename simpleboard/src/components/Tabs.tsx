2997c94e0c22664ca7ac4fa9a21dbeb257a9067a
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-07-13 16:06:26 +0900

    탭 정리

--- simpleboard/src/components/Tabs.tsx
+++ simpleboard/src/components/Tabs.tsx
@@ -12,19 +12,17 @@ function TabComponent() {
     <>
  
       <Tabs
-        defaultActiveKey="profile"
+        defaultActiveKey="home"
         id="uncontrolled-tab-example"
         className="mb-3"
       >
         <Tab eventKey="home" title="Home">
           Tab content for Home
         </Tab>
-        <Tab eventKey="profile" title="Profile">
-          Tab content for Profile
+        <Tab eventKey="mypage" title="My Page">
+          Tab content for Mypage
         </Tab>
-        <Tab eventKey="contact" title="Contact" disabled>
-          Tab content for Contact
-        </Tab>
+
       </Tabs>
       
     </>
